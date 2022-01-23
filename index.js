6e7b6c82464f13500e29f274e1c775fa5f8f199a
Andrea Monroy <andreamonroy@MacBook-Air-de-Andrea.local>
2022-01-23 14:53:59 -0500

    add cors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
 const app = express();
 const { config } = require('./config/index');
@@ -25,7 +25,7 @@ app.use(notFoundHandler);
 app.use(logErrors);
 app.use(wrapError);
 app.use(errorHandlers);
-app.use(cors());
+// app.use(cors());
 function setupCORS(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-type,Accept,X-Access-Token,X-Key');
